de7d576cbf5faa016b4a9df0e0fd4c8fbdbffd57
Vien Hoang <vieninsweden@gmail.com>
2014-01-13 13:06:09 +0100

    Update to bootstrap 3.0.3

--- _/components/js/tab.js
+++ _/components/js/tab.js
@@ -1,8 +1,8 @@
 /* ========================================================================
- * Bootstrap: tab.js v3.0.0
- * http://twbs.github.com/bootstrap/javascript.html#tabs
+ * Bootstrap: tab.js v3.0.3
+ * http://getbootstrap.com/javascript/#tabs
  * ========================================================================
- * Copyright 2012 Twitter, Inc.
+ * Copyright 2013 Twitter, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -132,4 +132,4 @@
     $(this).tab('show')
   })
 
-}(window.jQuery);
+}(jQuery);
